7c895e5b6876f13257148f07133d17b384f92976
Scott Guertin <sguertin@gmail.com>
2022-12-18 19:57:15 -0500

    Front end updates

--- UserManager/wwwroot/js/site.js
+++ UserManager/wwwroot/js/site.js
@@ -1,24 +1,92 @@
-﻿import Ajax from './ajax.js'
+class Ajax {
+  static ajax(url, method, headers = null, requestBody = null) {
+    if (method == 'GET') {
+      requestBody = null;
+    }
+    if (headers == null) {
+      headers = {}
+    }
+    headers['method'] = method;
 
-const companyList = document.getElementById('company'),
+    if (requestBody != null) {
+      requestBody = JSON.stringify(requestBody);
+    }
+    return fetch(url, {
+      method: method, // *GET, POST, PUT, DELETE, etc.
+      mode: 'same-origin', // no-cors, *cors, same-origin
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'same-origin', // include, *same-origin, omit
+      headers: {
+        'content-type': 'application/json'
+      },
+      redirect: 'follow', // manual, *follow, error
+      referrerPolicy: 'same-origin', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      body: requestBody // body data type must match "Content-Type" header
+    });
+  }
+
+  static get(url, headers = null) {
+    return this.ajax(url, 'GET', headers);
+  }
+
+  static post(url, body, headers = null) {    
+    return this.ajax(url, 'POST', headers, body);
+  }
+
+  static put(url, body, headers = null) {
+    return this.ajax(url, 'PUT', headers, body);
+  }
+
+  static delete(url, body, headers = null) {
+    return this.ajax(url, 'DELETE', headers, body);
+  }
+}
+
+const checkboxes = ['vpn', 'outlookMailbox', 'confidential'],
+    companyList = document.getElementById('company'),
     countryList = document.getElementById('country'),
     departmentList = document.getElementById('department'),
     locationList = document.getElementById('location'),
     createButton = document.getElementById('createBtn'),
-    loadDropdown = (select, items) => {
-        while (select.firstChild) {
-            select.removeChild(select.firstChild);
+    loadDropdown = (dropdownList, items) => {
+        while (dropdownList.firstChild) {
+            dropdownList.removeChild(dropdownList.firstChild);
         }
-        items.forEach(item => select.add(new Option(item.name, item.name)))
+        items.forEach(item => dropdownList.add(new Option(item.name, item.name)))
     },
     getData = () => {
-        let formData = new FormData(document.getElementById('form'))
-        var data = {};
-        formData.forEach((value, key) => data[key] = value);
+        let formData = new FormData(document.getElementById('form')),
+          data = {
+            firstName: null,
+            lastName: null,
+            shortName: null,
+            description: null,
+            displayName: null,
+            jobTitle: null,
+            country: null,
+            location: null,
+            company: null,
+            manager: null,
+            costCenter: null,
+            department: null,
+            vpn: false,
+            outlookMailbox: false,
+            confidential: false
+          };
+      formData.forEach((value, key) => {
+        if (checkboxes.includes(key)) {
+          data[key] = value == 'on';
+        } else {
+          data[key] = value
+        }        
+      });
         return data;
     };
 
 createButton.addEventListener('click', () => {
-    let formData = getData();
-    console.log(formData);
+  let formData = getData();
+  console.log(formData);
+  Ajax.post('/api/User', formData).then(r => {
+    console.log(r);
+  })
 })
